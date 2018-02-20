9b401a51dc228afd4787cefae2e281a93561392e
Jason Lewis <jason@dickson.st>
2018-02-20 13:07:20 +1100

    update questions

--- build-tests.sh
+++ build-tests.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #build test-1 with answers
-pdflatex "\def\showanswers{1}\input{maths-1.tex}" && cp maths-1.pdf ../maths-1-answers.pdf
+pdflatex "\def\showanswers{1}\input{maths-1.tex}" && cp maths-1.pdf maths-1-answers.pdf
 # build test-1 without answers
-pdflatex "\input{maths-1.tex}" && cp maths-1.pdf ../maths-1.pdf
+pdflatex "\input{maths-1.tex}" 
 
 
 #build maths-2 with answers
-pdflatex "\def\showanswers{1}\input{maths-2.tex}" && cp maths-2.pdf ../maths-2-answers.pdf
+pdflatex "\def\showanswers{1}\input{maths-2.tex}" && cp maths-2.pdf maths-2-answers.pdf
 # build test-1 without answers
-pdflatex "\input{maths-2.tex}" && cp maths-2.pdf ../maths-2.pdf
+pdflatex "\input{maths-2.tex}" 
