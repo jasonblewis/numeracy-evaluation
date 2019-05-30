1224b75d2ed301dca5f971ca0fbb5daf976128d1
Jason Lewis <jason@dickson.st>
2019-05-30 13:15:41 +1000

    keep q10 on same page as the table

--- build-tests.sh
+++ build-tests.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-#build test-1 with answers
-pdflatex "\def\showanswers{1}\input{maths-1.tex}" && cp maths-1.pdf maths-1-answers.pdf
-# build test-1 without answers
-pdflatex "\input{maths-1.tex}" 
+# requires latexmk - check it exists.
+if ! hash latexmk 2>/dev/null
+then
+    echo 'ERROR: requires latexmk'
+    exit 1
+fi
 
+build () {
+    # build with answers
+    ANSWERS='\def\showanswers{1}' latexmk -gg -pdf -shell-escape "$1".tex && cp "$1".pdf "$1"-answers.pdf
+    # build  without answers
+    latexmk -gg -pdf -shell-escape "$1".tex 
+}
 
-#build maths-2 with answers
-pdflatex "\def\showanswers{1}\input{maths-2.tex}" && cp maths-2.pdf maths-2-answers.pdf
-# build test-2 without answers
-pdflatex "\input{maths-2.tex}" 
+#build test-1 with and without answers
+build "maths-1"
+
+#build test-2 with and without answers
+build "maths-2"
+
